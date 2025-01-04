8b495c0dacdb7a3bebd8037d9f5732bc45b3989e
hedy-kh <hedykhkh@gmail.com>
2025-01-04 08:33:26 +0100

    Fix formatting issues

--- src/FundMe.sol
+++ src/FundMe.sol
@@ -6,6 +6,7 @@ import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/Ag
 //import {AggregatorV3Interface} from "lib/chainlink-brownie-contracts/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 import {PriceConverter} from "./PriceConverter.sol";
+
 error Fundeme__NotOwner();
 
 contract FundMe {
@@ -18,17 +19,19 @@ contract FundMe {
     address private immutable i_owner;
     uint256 public constant MINIMUM_USD = 5 * 10 ** 18;
     AggregatorV3Interface private priceFeed;
+
     constructor(address _ipricefeed) {
         i_owner = msg.sender;
         priceFeed = AggregatorV3Interface(_ipricefeed);
     }
+
     function fund() public payable {
         require(msg.value.getConversionRate(priceFeed) >= MINIMUM_USD, "You need to spend more ETH!");
         // require(PriceConverter.getConversionRate(msg.value) >= MINIMUM_USD, "You need to spend more ETH!");
         addressToAmountFunded[msg.sender] += msg.value;
         funders.push(msg.sender);
     }
-    
+
     function getVersion() public view returns (uint256) {
         return priceFeed.version();
     }
@@ -38,16 +41,18 @@ contract FundMe {
         if (msg.sender != i_owner) revert Fundeme__NotOwner();
         _;
     }
-    function cheaperWithDraw () public onlyOwner {
-      uint256  fundersLength = funders.length;
-      for (uint256 i =0; i< fundersLength ; i++){
-        address funder = funders[i];
-        addressToAmountFunded[funder] = 0;
-      }
-      funders = new address[](0);
-      (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}("");
-      require(callSuccess, "Call failed");
+
+    function cheaperWithDraw() public onlyOwner {
+        uint256 fundersLength = funders.length;
+        for (uint256 i = 0; i < fundersLength; i++) {
+            address funder = funders[i];
+            addressToAmountFunded[funder] = 0;
+        }
+        funders = new address[](0);
+        (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
     }
+
     function withdraw() public onlyOwner {
         for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
             address funder = funders[funderIndex];
@@ -84,14 +89,17 @@ contract FundMe {
     receive() external payable {
         fund();
     }
-    function getAddressToAmount(address _fundingaddress) external view returns (uint256){
-       return addressToAmountFunded[_fundingaddress];
+
+    function getAddressToAmount(address _fundingaddress) external view returns (uint256) {
+        return addressToAmountFunded[_fundingaddress];
     }
-    function getFunders (uint256 _index) external view returns(address){
+
+    function getFunders(uint256 _index) external view returns (address) {
         return funders[_index];
     }
-    function getOwner () external view returns (address){
-      return i_owner;
+
+    function getOwner() external view returns (address) {
+        return i_owner;
     }
 }
 
--- test/unit/FundMe.t.sol
+++ test/unit/FundMe.t.sol
@@ -1,116 +1,128 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import {Test,console} from 'forge-std/Test.sol';
+import {Test, console} from "forge-std/Test.sol";
 import {FundMe as FundContract} from "../../src/FundMe.sol";
-import {deployFundme as deployscript} from "../../script/DeployFundMe.s.sol" ;
+import {deployFundme as deployscript} from "../../script/DeployFundMe.s.sol";
+
 contract fundmetest is Test {
     FundContract Fundme;
     address User = makeAddr("user");
     uint256 constant SENDING_VALUE = 0.1 ether;
     uint256 constant USER_BALANCE = 10 ether;
     uint256 constant GAS_PRICE = 1;
-   function setUp() external{
-     deployscript Script = new deployscript();
-     Fundme = Script.run();
-     vm.deal(User,USER_BALANCE);
-   }
-   function testMinUsd() public {
-     assertEq(Fundme.MINIMUM_USD(),5e18);
-   }
-   function testOwnerIsSender() public {
-    console.log("owner address:",Fundme.getOwner());
-    console.log("contract address : ",address(this));
-    console.log(msg.sender);
-    //assertEq(Fundme.i_owner(),address(this)); address(this) if you deploy using the test 
-    assertEq(Fundme.getOwner(),msg.sender);//msg.sender if you do the script to deploy contract
-   }
-   function testVersion() public {
-    uint256 version = Fundme.getVersion();
-    console.log(version);
-    assertEq(version,4);
-   }
-   function testFundEth()public {
-    vm.expectRevert();
-    Fundme.fund();
-    
-   }
-   modifier getfund {
-    vm.prank(User); //next tx will be sent by this user (address)
-    Fundme.fund{value:SENDING_VALUE}();
-    _;
-   }
-   function testFundUpdate() public getfund {
-    // vm.prank(User); //next tx will be sent by this user (address)
-    // Fundme.fund{value:SENDING_VALUE}();
-     uint256 amountToFund = Fundme.getAddressToAmount(User);
-    assertEq(amountToFund,SENDING_VALUE);
-   }
-   function testFunders() public getfund {
-    address funder = Fundme.getFunders(0);
-    assertEq(funder,User);
-   }
-   function testOwnlyWithdraw() public getfund {
-    vm.expectRevert();
-    vm.prank(User);
-    Fundme.withdraw();
-   }
-   function testWithdrawSingle () public getfund {
-    uint256 startingBalanceOwner = Fundme.getOwner().balance;
-    uint256 startingFundMeBalance = address(Fundme).balance;
-    vm.prank(Fundme.getOwner());
-    Fundme.withdraw();
-    uint256 endingBalanceOwner = Fundme.getOwner().balance;
-    uint256 endingFundBalance = address(Fundme).balance;
-    assertEq(endingFundBalance,0);
-    assertEq(startingBalanceOwner+startingFundMeBalance,endingBalanceOwner);
-   }
-   function testWithMultiple () public {
-    uint160 FundersNumbers = 10;
-    uint160 Funderindex = 1;
-    
-    for (uint256 i ; i<FundersNumbers ;i++){
-      //hoax() = vm.prank + vm.deal;
-      hoax(address(1),SENDING_VALUE);
-      Fundme.fund{value:SENDING_VALUE}();
+
+    function setUp() external {
+        deployscript Script = new deployscript();
+        Fundme = Script.run();
+        vm.deal(User, USER_BALANCE);
     }
-    uint256 startingOwnerBalance = Fundme.getOwner().balance;
-    uint256 startingFundBalance = address(Fundme).balance;
-    uint256 gasStart = gasleft();
-    vm.txGasPrice(GAS_PRICE);
-    vm.prank(Fundme.getOwner());
-    Fundme.withdraw();
-    uint256 gasEnd = gasleft();
-    uint256 gasUsed = (gasStart - gasEnd) * tx.gasprice;
-    console.log("gasStaret :",gasStart);
-    console.log("gasEnd : ",gasEnd);
-    console.log("gasUsed : ",gasUsed);
-    assert (address(Fundme).balance==0);
-    
-    assert (startingFundBalance+startingOwnerBalance == Fundme.getOwner().balance);
-   }
-   function testWithMultipleCheaper () public {
-    uint160 FundersNumbers = 10;
-    uint160 Funderindex = 1;
-    
-    for (uint256 i ; i<FundersNumbers ;i++){
-      //hoax() = vm.prank + vm.deal;
-      hoax(address(1),SENDING_VALUE);
-      Fundme.fund{value:SENDING_VALUE}();
+
+    function testMinUsd() public {
+        assertEq(Fundme.MINIMUM_USD(), 5e18);
     }
-    uint256 startingOwnerBalance = Fundme.getOwner().balance;
-    uint256 startingFundBalance = address(Fundme).balance;
-    uint256 gasStart = gasleft();
-    vm.txGasPrice(GAS_PRICE);
-    vm.prank(Fundme.getOwner());
-    Fundme.cheaperWithDraw();
-    uint256 gasEnd = gasleft();
-    uint256 gasUsed = (gasStart - gasEnd) * tx.gasprice;
-    console.log("gasStaret :",gasStart);
-    console.log("gasEnd : ",gasEnd);
-    console.log("gasUsed : ",gasUsed);
-    assert (address(Fundme).balance==0);
-    
-    assert (startingFundBalance+startingOwnerBalance == Fundme.getOwner().balance);
-   }
+
+    function testOwnerIsSender() public {
+        console.log("owner address:", Fundme.getOwner());
+        console.log("contract address : ", address(this));
+        console.log(msg.sender);
+        //assertEq(Fundme.i_owner(),address(this)); address(this) if you deploy using the test
+        assertEq(Fundme.getOwner(), msg.sender); //msg.sender if you do the script to deploy contract
+    }
+
+    function testVersion() public {
+        uint256 version = Fundme.getVersion();
+        console.log(version);
+        assertEq(version, 4);
+    }
+
+    function testFundEth() public {
+        vm.expectRevert();
+        Fundme.fund();
+    }
+
+    modifier getfund() {
+        vm.prank(User); //next tx will be sent by this user (address)
+        Fundme.fund{value: SENDING_VALUE}();
+        _;
+    }
+
+    function testFundUpdate() public getfund {
+        // vm.prank(User); //next tx will be sent by this user (address)
+        // Fundme.fund{value:SENDING_VALUE}();
+        uint256 amountToFund = Fundme.getAddressToAmount(User);
+        assertEq(amountToFund, SENDING_VALUE);
+    }
+
+    function testFunders() public getfund {
+        address funder = Fundme.getFunders(0);
+        assertEq(funder, User);
+    }
+
+    function testOwnlyWithdraw() public getfund {
+        vm.expectRevert();
+        vm.prank(User);
+        Fundme.withdraw();
+    }
+
+    function testWithdrawSingle() public getfund {
+        uint256 startingBalanceOwner = Fundme.getOwner().balance;
+        uint256 startingFundMeBalance = address(Fundme).balance;
+        vm.prank(Fundme.getOwner());
+        Fundme.withdraw();
+        uint256 endingBalanceOwner = Fundme.getOwner().balance;
+        uint256 endingFundBalance = address(Fundme).balance;
+        assertEq(endingFundBalance, 0);
+        assertEq(startingBalanceOwner + startingFundMeBalance, endingBalanceOwner);
+    }
+
+    function testWithMultiple() public {
+        uint160 FundersNumbers = 10;
+        uint160 Funderindex = 1;
+
+        for (uint256 i; i < FundersNumbers; i++) {
+            //hoax() = vm.prank + vm.deal;
+            hoax(address(1), SENDING_VALUE);
+            Fundme.fund{value: SENDING_VALUE}();
+        }
+        uint256 startingOwnerBalance = Fundme.getOwner().balance;
+        uint256 startingFundBalance = address(Fundme).balance;
+        uint256 gasStart = gasleft();
+        vm.txGasPrice(GAS_PRICE);
+        vm.prank(Fundme.getOwner());
+        Fundme.withdraw();
+        uint256 gasEnd = gasleft();
+        uint256 gasUsed = (gasStart - gasEnd) * tx.gasprice;
+        console.log("gasStaret :", gasStart);
+        console.log("gasEnd : ", gasEnd);
+        console.log("gasUsed : ", gasUsed);
+        assert(address(Fundme).balance == 0);
+
+        assert(startingFundBalance + startingOwnerBalance == Fundme.getOwner().balance);
+    }
+
+    function testWithMultipleCheaper() public {
+        uint160 FundersNumbers = 10;
+        uint160 Funderindex = 1;
+
+        for (uint256 i; i < FundersNumbers; i++) {
+            //hoax() = vm.prank + vm.deal;
+            hoax(address(1), SENDING_VALUE);
+            Fundme.fund{value: SENDING_VALUE}();
+        }
+        uint256 startingOwnerBalance = Fundme.getOwner().balance;
+        uint256 startingFundBalance = address(Fundme).balance;
+        uint256 gasStart = gasleft();
+        vm.txGasPrice(GAS_PRICE);
+        vm.prank(Fundme.getOwner());
+        Fundme.cheaperWithDraw();
+        uint256 gasEnd = gasleft();
+        uint256 gasUsed = (gasStart - gasEnd) * tx.gasprice;
+        console.log("gasStaret :", gasStart);
+        console.log("gasEnd : ", gasEnd);
+        console.log("gasUsed : ", gasUsed);
+        assert(address(Fundme).balance == 0);
+
+        assert(startingFundBalance + startingOwnerBalance == Fundme.getOwner().balance);
+    }
 }
